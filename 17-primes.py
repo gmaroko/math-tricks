09ef5d9dc9cf383e0c16fec8c4d885c2ee5e4dab
unknown <marokogideon@gmail.com>
2018-08-02 01:04:23 +0300

    main

--- 17-primes.py
+++ 17-primes.py
@@ -7,14 +7,31 @@ def primes(target):
         numbers.difference_update(set(range(_p*2, target+1, _p)))
     return _primes
 
+def x(n):
+    while True:
+        for i in range(n):
+            yield i
+    x(n-1)
 
 def main():
     from itertools import combinations_with_replacement as c
     TARGET = 17
-    _primes = primes(TARGET)
-    for i in range(1, len(_primes)+1):
-        temp_item = [list(item) for item in c(_primes, i)]
-        x=[]
-    print(temp_item)
+    _pri = [2,3,5,7,11,13,17] #primes(TARGET)
+    true_sums = []
+    done = False
+    index = 0
+    while not done:
+        list_item = [list(item) for item in c(_pri, index)]
+        for a_sub_list in list_item:
+            if sum(a_sub_list) == 17:
+                print(a_sub_list)
+                true_sums.append(a_sub_list)
+            else:
+                pass
+        index+=1
+        if index>len(_pri)+1:
+            done = True
+    assert len(true_items) == 17
+    print("Agreed")
 
 main()
