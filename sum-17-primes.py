097db6575924a9df23d9d4c1f22bd3ac26c241bd
unknown <marokogideon@gmail.com>
2018-08-02 01:38:19 +0300

    renamed 17-primes.py

---
+++ sum-17-primes.py
@@ -0,0 +1,30 @@
+def func():
+    from itertools import combinations_with_replacement as c
+    TARGET = 17
+    #prime numbers >=17
+    numbers = set(range(TARGET, 1, -1))
+    _primes = []
+    while numbers:
+        _p = numbers.pop()
+        _primes.append(_p)
+        numbers.difference_update(set(range(_p*2, TARGET+1, _p)))
+    #sums
+    true_sums, done, index = [], False, 0
+    while not done:
+        main_list = [list(item) for item in c(_primes, index)]
+        for a_sub_list in main_list:
+            if sum(a_sub_list) == TARGET:
+                true_sums.append(a_sub_list)
+            else:
+                pass
+        index+=1
+        if index > len(_primes)+1:
+            done = True
+    assert len(true_sums) == TARGET
+    for true_s in true_sums:
+        temp = ''
+        for i in true_s:
+            temp = temp+str(i)+'+'
+        print("{} = {}".format(temp[:len(temp)-1], TARGET))
+    print("\nCount: {}\nAgreed".format(len(true_sums)))
+func()
